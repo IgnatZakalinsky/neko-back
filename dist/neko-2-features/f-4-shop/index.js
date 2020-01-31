a0c7d0420bd4363c3241f0a3c1578c13a0642e11
ai73aaa <1qazxcvBG>
2020-01-31 13:50:07 +0300

    add shop put

--- dist/neko-2-features/f-4-shop/index.js
+++ dist/neko-2-features/f-4-shop/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const shopGet_1 = require("./s-1-controllers/shopGet");
 const shopPost_1 = require("./s-1-controllers/shopPost");
+const shopPut_1 = require("./s-1-controllers/shopPut");
 const shop = express_1.default.Router();
 shopGet_1.shopGet('/', shop);
 shopPost_1.shopPost('/', shop);
+shopPut_1.shopPut('/', shop);
 exports.default = shop;
 //# sourceMappingURL=index.js.map
--- src/neko-2-features/f-4-shop/index.ts
+++ src/neko-2-features/f-4-shop/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {shopGet} from "./s-1-controllers/shopGet";
 import {shopPost} from "./s-1-controllers/shopPost";
+import {shopPut} from "./s-1-controllers/shopPut";
 
 const shop = express.Router();
 
 shopGet('/', shop);
 shopPost('/', shop);
+shopPut('/', shop);
 
 export default shop;
